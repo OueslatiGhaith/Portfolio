4658f3e348da3f5df3c5ed898e21755b38a4b643
OueslatiGhaith <73850124+OueslatiGhaith@users.noreply.github.com>
2020-12-14 22:53:05 +0100

    small fixes
    fixed some small visual bugs and added some missing files

--- JS/Theme.js
+++ JS/Theme.js
@@ -1,15 +1,14 @@
 //DOM elements
 const darkButton = document.getElementById('dark')
 const lightButton = document.getElementById('light')
-const solarButton = document.getElementById('solalr')
+
 const body = document.body;
 // cashe
 const theme = localStorage.getItem('theme');
-const isSolar = localStorage.getItem('isSolar');
 
 if (theme) {
     body.classList.add(theme);
-    isSolar && body.classList.add('solar');
+    body.classList.add('solar');
 }
 //button event handler
 darkButton.onclick = () => {
@@ -19,15 +18,4 @@ darkButton.onclick = () => {
 lightButton.onclick = () => {
     body.classList.replace('dark', 'light')
     localStorage.setItem('theme', 'light');
-};
-solarButton.onclick = () => {
-    if (body.classList.contains('solar')) {
-        body.classList.remove('solar');
-        solarButton.innerText = 'solarize';
-        localStorage.removeItem('isSolar');
-    } else {
-        body.classList.add('solar');
-        solarButton.innerText = 'normalize';
-        localStorage.add('isSolar', true);
-    }
 };
