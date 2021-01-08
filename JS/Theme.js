e25e71c7bc978c15334c42ae98e4ea6e8f4af988
OueslatiGhaith <73850124+OueslatiGhaith@users.noreply.github.com>
2021-01-08 19:18:49 +0100

    fsqs
    sqffqsf

--- JS/Theme.js
+++ JS/Theme.js
@@ -1,21 +1,20 @@
 //DOM elements
-const darkButton = document.getElementById('dark')
-const lightButton = document.getElementById('light')
+const darkButton = document.getElementById("dark");
+const lightButton = document.getElementById("light");
 
 const body = document.body;
 // cashe
-const theme = localStorage.getItem('theme');
+const theme = localStorage.getItem("theme");
 
 if (theme) {
-    body.classList.add(theme);
-    body.classList.add('solar');
+  body.classList.add(theme);
 }
 //button event handler
 darkButton.onclick = () => {
-    body.classList.replace('light', 'dark')
-    localStorage.setItem('theme', 'dark');
+  body.classList.replace("light", "dark");
+  localStorage.setItem("theme", "dark");
 };
 lightButton.onclick = () => {
-    body.classList.replace('dark', 'light')
-    localStorage.setItem('theme', 'light');
+  body.classList.replace("dark", "light");
+  localStorage.setItem("theme", "light");
 };
